b5574c97c67806e630e35cc8ccdc19ed8e06244e
MatBylin <matbylin@gmail.com>
2026-04-25 18:06:25 +0200

    TableCell - independent from Text element

--- gui/src/main/java/org/matbylin/gui/core/elements/TableCell.java
+++ gui/src/main/java/org/matbylin/gui/core/elements/TableCell.java
@@ -2,11 +2,20 @@ package org.matbylin.gui.core.elements;
 
 import com.codeborne.selenide.SelenideElement;
 import lombok.extern.slf4j.Slf4j;
+import org.matbylin.gui.core.elements.base.BaseElement;
+
+import static com.codeborne.selenide.Condition.visible;
 
 @Slf4j
-public class TableCell extends Text {
+public class TableCell extends BaseElement<TableCell> {
 
     public TableCell(SelenideElement element) {
         super(element);
+    }
+
+    public String getText() {
+        log.info("Returning text from element '{}'", getElement());
+        waitForReadyState();
+        return getElement().shouldBe(visible).getText();
     }
 }
