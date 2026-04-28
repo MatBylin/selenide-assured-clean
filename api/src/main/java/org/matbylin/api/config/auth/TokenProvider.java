2ee38855fb4a72e471d43a2d333820495ba56c6e
MatBylin <matbylin@gmail.com>
2026-04-28 13:09:08 +0200

    Put api token into env.properties

--- api/src/main/java/org/matbylin/api/config/auth/TokenProvider.java
+++ api/src/main/java/org/matbylin/api/config/auth/TokenProvider.java
@@ -1,10 +1,11 @@
 package org.matbylin.api.config.auth;
 
+
 import org.matbylin.core.config.EnvironmentConfigProvider;
 
 public class TokenProvider implements AuthProvider {
     @Override
     public String getToken() {
-        return "";
+        return EnvironmentConfigProvider.get().apiAppToken();
     }
 }
